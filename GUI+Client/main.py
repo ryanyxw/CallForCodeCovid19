a5ed092e8c53143e0f69084bc4689d9c62c101b4
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-07-28 00:00:00 +0800

    Replacing Client.py w/ Client_Experimental.py

--- GUI+Client/main.py
+++ GUI+Client/main.py
@@ -25,7 +25,7 @@ import sys
 #Regular Expressions
 import re
 #Client
-import client_experimental as client
+import client
 #network interfaces
 import netifaces
 #Using a for loop to continue requests if the request failed
