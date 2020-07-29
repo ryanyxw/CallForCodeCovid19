93691ee82bf1c302ed2a88c47fbef48ef115e95b
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-07-29 17:49:40 +0800

    Copyright Fix

--- GUI+Client/main.py
+++ GUI+Client/main.py
@@ -28,6 +28,24 @@ import client
 #network interfaces
 import netifaces
 
+"""
+License:
+
+   Copyright 2020 Ryan Wang and Tyllis Xu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 #Creates a .CovidContactTracer directory to store logs and local files
 this = sys.modules[__name__]
 if platform != 'android':
@@ -781,7 +799,7 @@ WindowManager:
         pos_hint: {"center_x": 0.5, "bottom": 0}
         text: root.store.get("homeLabel")["value"]
         size_hint: 1, 0.1
-    
+
 
 <SideBarPage>:
     name: "sidebar"
